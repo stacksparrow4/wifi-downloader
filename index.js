08a0acfcb7fe6c7d1de91beedadca2905faa6834
Daniel Cooper <moodragon46@gmail.com>
2019-12-31 10:07:05 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const electron = require('electron');
+const path = require('path');
+const dns = require('dns');
+const os = require('os');
+
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+
+let mainWin;
+let server;
+
+function getIp(){
+    return new Promise(res => {
+        dns.lookup(os.hostname(), function (err, add, fam) {
+            res(add);
+        })
+    })
+}
+
+ipcMain.on('startserver', () => {
+    const app = express();
+    const port = 5000;
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'www/downloadpage.html'));
+    })
+
+    server = app.listen(port, () => {
+        getIp().then(ip => {
+            mainWin.webContents.send('serverstarted', ip, port);
+        })
+    })
+})
+
+ipcMain.on('stopserver', () => {
+    if(server !== undefined){
+        server.close();
+        server = undefined;
+    }
+
+    mainWin.webContents.send('serverstopped');
+})
+
+app.on('ready', () => {
+    mainWin = new BrowserWindow({
+        width: 600,
+        height: 400,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWin.on('closed', () => {
+        mainWin = null;
+        app.quit();
+    })
+
+    mainWin.loadFile(path.join(__dirname, 'www/wifidownloader.html'));
+
+    const menu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(menu);
+})
+
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Quit',
+                click: () => {
+                    app.quit();
+                }
+            }
+        ]
+    },
+    {
+        label: 'Server'
+    }
+]
+if(process.env.NODE_ENV !== 'production'){
+    menuTemplate.push({
+        label: 'Dev',
+        submenu: [
+            {
+                label: 'Devtools',
+                accelerator: 'Ctrl+Shift+I',
+                click: (item, focusedWin) => {
+                    focusedWin.toggleDevTools();
+                }
+            }
+        ]
+    })
+}
---
+++ www/main.js
@@ -0,0 +1,38 @@
+const electron = window.require('electron');
+const { ipcRenderer } = electron;
+
+
+const startServerBtn = document.getElementById('startserver');
+const status = document.getElementById('status');
+const downloadUrl = document.getElementById('downloadurl');
+
+let serverRunning = false;
+let waitingForResp = false;
+
+startServerBtn.addEventListener('click', () => {
+    if(serverRunning){
+        ipcRenderer.send('stopserver');
+        waitingForResp = true;
+    }else {
+        ipcRenderer.send('startserver');
+        waitingForResp = true;
+    }
+    startServerBtn.innerText = 'Starting server...';
+})
+
+ipcRenderer.on('serverstarted', (e, ip, port) => {
+    waitingForResp = false;
+    serverRunning = true;
+    startServerBtn.innerText = 'Stop server';
+
+    const url = `${ip}:${port}`;
+
+    status.innerHTML = `<p>Server is now running. Go to:</p><a href="#">${url}</a><p>on any device that is connected to the same wifi network as you.</p>`;
+})
+
+ipcRenderer.on('serverstopped', () => {
+    waitingForResp = false;
+    serverRunning = false;
+    startServerBtn.innerText = 'Start server';
+    status.innerHTML = '';
+})
